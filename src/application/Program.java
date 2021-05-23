9aa922e2bb7c59338048b4afaefa9eb55c8e7aab
Bruno Brilhante <brunobrilhante53@gmail.com>
2021-05-22 23:16:14 -0300

    Ajustando as classes e adicionando os cálculos

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,38 +2,113 @@ package application;
 
 import java.util.Locale;
 
+import entities.Copo;
+import entities.Jarra;
+import entities.Prato;
 import entities.Taca;
 
 public class Program {
 
 	public static void main(String[] args) {
 			
-        Locale.setDefault(Locale.US);
-		
-        /*
-        
-		double[] base = new double[2000]; 
-		
-		for(int i = 0; i < 2000; i++) {
-			Random r = new Random();
-			double randomValue = 0.8 + (1.5 - 0.8) * r.nextDouble();
-			base[i] = randomValue;
-		}
-		
-        for(Double d : base) {
-        	System.out.format("%.2f\n", d);
-        }
-        
-		*/
+        Locale.setDefault(Locale.US);		
         
         Taca taca = new Taca();
+        Jarra jarra = new Jarra();
+        Prato prato = new Prato();
+        Copo copo = new Copo();
         
-        //System.out.format("%d", taca.passoUm());
-        taca.passoUm();
+        taca.gerarNumeros();             
+        jarra.gerarNumeros();
+        copo.gerarNumeros();
+        prato.gerarNumeros();
         
-        System.out.println("--------------");
+        int acertosTaca = 0;
+        int errosTaca = 0;
         
-        taca.passoUm();
+        for(int i = 0; i < 1000; i++) {        	        
+	        double acertoTaca = Math.abs((taca.espessuraTreinamento[i] - taca.mediaEspessura()) + (taca.formaTreinamento[i] - taca.mediaForma()));		               
+	        double erroJarra = Math.abs((jarra.espessuraTreinamento[i] - jarra.mediaEspessura()) + (jarra.formaTreinamento[i] - jarra.mediaForma()));        	        
+	        double erroPrato = Math.abs((prato.espessuraTreinamento[i] - prato.mediaEspessura()) + (prato.formaTreinamento[i] - prato.mediaForma()));        	        
+	        double erroCopo = Math.abs((copo.espessuraTreinamento[i] - copo.mediaEspessura()) + (copo.formaTreinamento[i] - copo.mediaForma()));	        
+	        
+	        if(acertoTaca < erroJarra && acertoTaca < erroPrato && acertoTaca < erroCopo) {
+	        	acertosTaca += 1;
+	        }
+	        else {
+	        	errosTaca += 1;
+	        }        
+        }
+        
+        System.out.println("Acertos Taça: " + acertosTaca);
+        System.out.println("Erros Taça: " + errosTaca + "\n");
+        
+        /* ------------------------------------------------------------------ */
+        
+        int acertosPrato = 0;
+        int errosPrato = 0;
+        
+        for(int i = 0; i < 1000; i++) {        	        
+	        double acertoPrato = Math.abs((prato.espessuraTreinamento[i] - prato.mediaEspessura()) + (prato.formaTreinamento[i] - prato.mediaForma())); 
+	        double erroJarra = Math.abs((jarra.espessuraTreinamento[i] - jarra.mediaEspessura()) + (jarra.formaTreinamento[i] - jarra.mediaForma()));        	        
+	        double erroTaca = Math.abs((taca.espessuraTreinamento[i] - taca.mediaEspessura()) + (taca.formaTreinamento[i] - taca.mediaForma()));		                 	        
+	        double erroCopo = Math.abs((copo.espessuraTreinamento[i] - copo.mediaEspessura()) + (copo.formaTreinamento[i] - copo.mediaForma()));	        
+	        
+	        if(acertoPrato < erroJarra && acertoPrato < erroTaca && acertoPrato < erroCopo) {
+	        	acertosPrato += 1;
+	        }
+	        else {
+	        	errosPrato += 1;
+	        }        
+        }
+        
+       System.out.println("Acertos Prato: " + acertosPrato);
+       System.out.println("Erros Prato: " + errosPrato + "\n");
+        
+       /* ------------------------------------------------------------------ */
+       
+       int acertosJarra = 0;
+       int errosJarra = 0;
+       
+       for(int i = 0; i < 1000; i++) {        	        
+	        double acertoJarra = Math.abs((jarra.espessuraTreinamento[i] - jarra.mediaEspessura()) + (jarra.formaTreinamento[i] - jarra.mediaForma())); 	        		 
+	        double erroPrato = Math.abs((prato.espessuraTreinamento[i] - prato.mediaEspessura()) + (prato.formaTreinamento[i] - prato.mediaForma()));        	        
+	        double erroTaca = Math.abs((taca.espessuraTreinamento[i] - taca.mediaEspessura()) + (taca.formaTreinamento[i] - taca.mediaForma()));		                 	        
+	        double erroCopo = Math.abs((copo.espessuraTreinamento[i] - copo.mediaEspessura()) + (copo.formaTreinamento[i] - copo.mediaForma()));	        
+	        
+	        if(acertoJarra < erroPrato && acertoJarra < erroTaca && acertoJarra < erroCopo) {
+	        	acertosJarra += 1;
+	        }
+	        else {
+	        	errosJarra += 1;
+	        }        
+       }
+       
+       System.out.println("Acertos Jarra: " + acertosJarra);
+       System.out.println("Erros Jarra: " + errosJarra + "\n");
+	
+       /* ------------------------------------------------------------------ */
+       
+       int acertosCopo = 0;
+       int errosCopo = 0;
+       
+       for(int i = 0; i < 1000; i++) {        	        
+	        double acertoCopo = Math.abs((copo.espessuraTreinamento[i] - copo.mediaEspessura()) + (copo.formaTreinamento[i] - copo.mediaForma())); 
+	        double erroPrato = Math.abs((prato.espessuraTreinamento[i] - prato.mediaEspessura()) + (prato.formaTreinamento[i] - prato.mediaForma()));        	        
+	        double erroTaca = Math.abs((taca.espessuraTreinamento[i] - taca.mediaEspessura()) + (taca.formaTreinamento[i] - taca.mediaForma()));		                 	        
+	        double erroJarra = Math.abs((jarra.espessuraTreinamento[i] - jarra.mediaEspessura()) + (jarra.formaTreinamento[i] - jarra.mediaForma()));	        
+	        
+	        if(acertoCopo < erroPrato && acertoCopo < erroTaca && acertoCopo < erroJarra) {
+	        	acertosCopo += 1;
+	        }
+	        else {
+	        	errosCopo += 1;
+	        }
+       }
+       
+       System.out.println("Acertos Copo: " + acertosCopo);
+       System.out.println("Erros Copo: " + errosCopo + "\n");  
+	
 	}
 
 }
--- src/entities/Taca.java
+++ src/entities/Taca.java
@@ -4,14 +4,14 @@ import java.util.Random;
 
 public class Taca {
 	
-	private Double[] espessuraTreinamento = new Double[1000];
-	private Double[] formaTreinamento = new Double[1000];
+	public Double[] espessuraTreinamento = new Double[1000];
+	public Double[] formaTreinamento = new Double[1000];
 	private Double[] espPrototipo = new Double[1000];
-	private Double[] formPrototipo = new Double[1000];	
+	private Double[] formPrototipo = new Double[1000];
 
 	Random r = new Random();
 	
-	public void passoUm() {
+	public void gerarNumeros() {
 		if(espessuraTreinamento[0] == null) {
 			for(int i = 0; i < 1000; i++) {			
 				double randomValue = 0.8 + (1.3 - 0.8) * r.nextDouble();
@@ -23,20 +23,24 @@ public class Taca {
 				formaTreinamento[i] = randomValue;
 				formPrototipo[i] = randomValue;
 			}
-			double somaEsp = 0;
-			double somaForm = 0;
-			for(int i = 0; i < 1000; i++) {
-				somaEsp += espPrototipo[i];
-				somaForm += formPrototipo[i];
-			}
-			double mediaEsp = somaEsp / espPrototipo.length;
-			double mediaForm = somaForm / formPrototipo.length;								
+
+		}
+	}
+
+	public double mediaForma() {		
+		double somaForm = 0;
+		for(int i = 0; i < 1000; i++) {			
+			somaForm += formPrototipo[i];
 		}		
-		for(Double d : espessuraTreinamento) {
-        	System.out.format("%.2f\n", d);
-        }
-		
-		
+		return somaForm / formPrototipo.length;	
+	}
+	
+	public double mediaEspessura() {		
+		double somaEsp = 0;
+		for(int i = 0; i < 1000; i++) {			
+			somaEsp += espPrototipo[i];
+		}		
+		return somaEsp / espPrototipo.length;	
 	}
 	
 	
